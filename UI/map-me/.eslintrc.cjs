a52ac238edf7d6ca608453e61ff3410caa29f371
calebbarzee <barzeec@gmail.com>
2023-10-06 17:13:29 -0600

    react-query and docker setup

--- UI/map-me/.eslintrc.cjs
+++ UI/map-me/.eslintrc.cjs
@@ -1,30 +1,36 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-  ],
-  rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
-    "@typescript-eslint/array-type": "off",
-    "@typescript-eslint/consistent-type-definitions": "off",
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		project: true,
+	},
+	plugins: ["@typescript-eslint"],
+	extends: [
+		"next/core-web-vitals",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:@typescript-eslint/stylistic-type-checked",
+		"plugin:@tanstack/eslint-plugin-query/recommended",
+	],
+	rules: {
+		// These opinionated rules are enabled in stylistic-type-checked above.
+		// Feel free to reconfigure them to your own preference.
+		"@typescript-eslint/array-type": "off",
+		"@typescript-eslint/consistent-type-definitions": "off",
 
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-  },
+		"@typescript-eslint/consistent-type-imports": [
+			"warn",
+			{
+				prefer: "type-imports",
+				fixStyle: "inline-type-imports",
+			},
+		],
+		"@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+		
+		// Added rules
+		"quotes": ["warn", "double", { "avoidEscape": true }],
+		"indent": ["warn", "tab", { "SwitchCase": 1 }],
+		"@typescript-eslint/indent": ["warn", "tab", { "SwitchCase": 1, "flatTernaryExpressions": true }],
+	},
 };
 
 module.exports = config;
--- UI/map-me/src/pages/_app.tsx
+++ UI/map-me/src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { QueryProvider } from "./api/query/Providers";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<QueryProvider>
+				<Component {...pageProps} />
+			</QueryProvider>
+		</SessionProvider>
+	);
 };
 
 export default MyApp;
