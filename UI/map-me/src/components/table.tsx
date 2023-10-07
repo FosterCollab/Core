9904cd6d678d8d35b31fc1c399da174eebbd0227
calebbarzee <barzeec@gmail.com>
2023-10-07 10:21:03 -0600

    clean up full

--- UI/map-me/src/components/table.tsx
+++ UI/map-me/src/components/table.tsx
@@ -1,13 +1,3 @@
-import {
-	Table,
-	TableHead,
-	TableRow,
-	TableHeaderCell,
-	TableBody,
-	TableCell,
-	Text
-} from "@tremor/react";
-
 interface User {
 	id: number;
 	name: string;
@@ -15,29 +5,29 @@ interface User {
 	email: string;
 }
 
-export default function UsersTable({ users }: { users: User[] }) {
-	return (
-		<Table>
-			<TableHead>
-				<TableRow>
-					<TableHeaderCell>Name</TableHeaderCell>
-					<TableHeaderCell>Username</TableHeaderCell>
-					<TableHeaderCell>Email</TableHeaderCell>
-				</TableRow>
-			</TableHead>
-			<TableBody>
-				{users.map((user) => (
-					<TableRow key={user.id}>
-						<TableCell>{user.name}</TableCell>
-						<TableCell>
-							<Text>{user.username}</Text>
-						</TableCell>
-						<TableCell>
-							<Text>{user.email}</Text>
-						</TableCell>
-					</TableRow>
-				))}
-			</TableBody>
-		</Table>
-	);
-}
+// export default function UsersTable({ users }: { users: User[] }) {
+// 	return (
+// 		<Table>
+// 			<TableHead>
+// 				<TableRow>
+// 					<TableHeaderCell>Name</TableHeaderCell>
+// 					<TableHeaderCell>Username</TableHeaderCell>
+// 					<TableHeaderCell>Email</TableHeaderCell>
+// 				</TableRow>
+// 			</TableHead>
+// 			<TableBody>
+// 				{users.map((user) => (
+// 					<TableRow key={user.id}>
+// 						<TableCell>{user.name}</TableCell>
+// 						<TableCell>
+// 							<Text>{user.username}</Text>
+// 						</TableCell>
+// 						<TableCell>
+// 							<Text>{user.email}</Text>
+// 						</TableCell>
+// 					</TableRow>
+// 				))}
+// 			</TableBody>
+// 		</Table>
+// 	);
+// }
