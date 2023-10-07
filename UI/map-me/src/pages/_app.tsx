b94eaf3112a0b8b8aad2a4368de7de8085882264
calebbarzee <barzeec@gmail.com>
2023-10-07 10:05:26 -0600

    partial clean

--- UI/map-me/src/pages/_app.tsx
+++ UI/map-me/src/pages/_app.tsx
@@ -2,6 +2,9 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { QueryProvider } from "./api/query/Providers";
+import { Suspense } from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer"
 
 import "~/styles/globals.css";
 
@@ -12,7 +15,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 	return (
 		<SessionProvider session={session}>
 			<QueryProvider>
+				<Suspense>
+					<Navbar />
+				</Suspense>
 				<Component {...pageProps} />
+				<Footer />
 			</QueryProvider>
 		</SessionProvider>
 	);
