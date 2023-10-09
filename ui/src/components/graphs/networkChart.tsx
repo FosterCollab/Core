c5ddbc05c4ba237b6f10c0170d895122555cc5f9
calebbarzee <barzeec@gmail.com>
2023-10-08 21:15:32 -0600

    navbar, project listing and search

--- ui/src/components/graphs/networkChart.tsx
+++ ui/src/components/graphs/networkChart.tsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Graph } from '@visx/network';
-import { hierarchy } from '@visx/hierarchy';
+import React from "react"
+import { Graph } from "@visx/network";
+import { hierarchy } from "@visx/hierarchy";
 
 const data = hierarchy({ count: Math.random() * 50 });
 
 const LinkNodeGraph: React.FC = () => {
-  return (
-    <div style={{ width: '800px', height: '600px' }}>
-      <Graph graph={data} linkComponent={({ link }) => (
-        <line
-          strokeWidth={1}
-          stroke="#999"
-          x1={link.source.x}
-          y1={link.source.y}
-          x2={link.target.x}
-          y2={link.target.y}
-        />
-      )} nodeComponent={({ node }) => (
-        <circle
-          r={15}
-          fill="#42f5d7"
-          cx={node.x}
-          cy={node.y}
-        />
-      )} />
-    </div>
-  );
+	return (
+		<div style={{ width: "800px", height: "600px" }}>
+			<Graph graph={data} linkComponent={({ link }) => (
+				<line
+					strokeWidth={1}
+					stroke="#999"
+					x1={link.source.x}
+					y1={link.source.y}
+					x2={link.target.x}
+					y2={link.target.y}
+				/>
+			)} nodeComponent={({ node }) => (
+				<circle
+					r={15}
+					fill="#42f5d7"
+					cx={node.x}
+					cy={node.y}
+				/>
+			)} />
+		</div>
+	);
 }
 
 export default LinkNodeGraph;
--- ui/src/pages/projects/demo.tsx
+++ ui/src/pages/projects/demo.tsx
@@ -1,46 +1,79 @@
-import { FC, useEffect, useState } from 'react';
+import { type FC, useState, useEffect } from "react";
+import fs from "fs";
+import path from "path";
 
 interface Project {
-  projectName: string;
-  peopleCount: string;
-  description: string;
-  datePosted: string;
+	name: string;
+	link: string;
+	label: string;
+	description: string;
 }
 
-const Projects: FC = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
+interface ProjectsProps {
+	projects: Project[];
+}
 
-  useEffect(() => {
-    const fetchProjects = async () => {
-      try {
-        const response = await fetch('/api/getProjects');
-        const data: Project[] = await response.json();
-        setProjects(data);
-      } catch (error) {
-        console.error("Failed to fetch projects:", error);
-      }
-    };
+const Projects: FC<ProjectsProps> = ({ projects }) => {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [filteredProjects, setFilteredProjects] = useState<Project[]>(projects);
 
-    fetchProjects();
-  }, []);
+	useEffect(() => {
+		const results = projects.filter(project => {
+			const { name, description, label } = project;
 
-  return (
-    <div className="container mx-auto p-6 max-w-3xl">
-        <br /><br /><br /><br />
-      <h2 className="text-3xl font-bold mb-10 text-center">Projects you can join</h2>
-      <div className="flex flex-col space-y-6 px-4 lg:px-16">
-        {projects.map((project, idx) => (
-          <div key={idx} className="bg-white p-6 rounded-lg shadow-lg">
-            <h3 className="text-xl font-semibold mb-2">{project.projectName}</h3>
-            <p><strong>People Count:</strong> {project.peopleCount}</p>
-            <p><strong>Date Posted:</strong> {project.datePosted}</p>
-            <p className="mt-2">{project.description}</p>
-          </div>
-        ))}
-      </div>
-      <br /><br /><br /><br />
-    </div>
-);
+			if (typeof name !== "string" || typeof description !== "string" || typeof label !== "string") {
+				return false;
+			}
+
+			return (
+				name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        label.toLowerCase().includes(searchTerm.toLowerCase())
+			);
+		});
+
+		setFilteredProjects(results);
+	}, [searchTerm, projects]);
+
+	return (
+		<div className="container mx-auto p-6 max-w-6xl">
+			<h2 className="text-3xl font-bold mb-10 text-center">Projects you can join</h2>
+			<div className="mb-6">
+				<input 
+					type="text"
+					value={searchTerm}
+					onChange={(e) => setSearchTerm(e.target.value)}
+					placeholder="Search projects..."
+					className="w-full p-2 border rounded"
+				/>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4 lg:px-16">
+				{filteredProjects.map((project, idx) => (
+					<div key={idx} className="bg-white p-6 rounded-lg shadow-lg">
+						<h3 className="text-xl font-semibold mb-2">{project.name}</h3>
+						<p className="mb-2">{project.label}</p>
+						<p className="mb-4">{project.description}</p>
+						<a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Learn More</a>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+}
+
+export async function getServerSideProps() {
+	try {
+		// Path to the JSON file within the public directory
+		const filePath = path.join(process.cwd(), "public", "demoData", "projects.json");
+
+		// Read data from the file
+		const data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+
+		return { props: { projects: data } };
+	} catch (error) {
+		console.error("Failed to fetch projects:", error);
+		return { props: { projects: [] } };
+	}
 }
 
 export default Projects;
